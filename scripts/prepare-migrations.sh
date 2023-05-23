421f7f1c5ffcc5a6839c22975d83ce6c03015fb7
Dmitry Momot <mail@dmomot.com>
2023-05-23 23:09:47 +0300

    fixed migrations path

--- scripts/prepare-migrations.sh
+++ scripts/prepare-migrations.sh
@@ -3,4 +3,4 @@
 ## Prepare migrations: clean old files and copy all migrations from services to migrations folder.
 mkdir -p ./migrations
 rm -Rvf $(find ./migrations -name "*.sql" ! -name "00000000000000-db_preparing.sql")
-cp -Rvf ./internal/**/repository/sql/migrations/*.sql ./migrations/
+cp -Rvf ./internal/repository/sql/migrations/*.sql ./migrations/
